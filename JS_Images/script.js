1d7d77b9152a4db99c1ff2f5a81060f1015750f8
chrissycodes2020 <61220882+chrissycodes2020@users.noreply.github.com>
2020-05-27 11:04:51 -0700

    Update
    Java Images trying to fix prompt

--- JS_Images/script.js
+++ JS_Images/script.js
@@ -1,12 +1,14 @@
-var response = prompt("Hello. " + "Whats your name?");
+var response = prompt("Hello. What's your name?");
 var myHeader = document.getElementById("header1");
 var hColorChangeButton = document.getElementById("headerControlButton");
+
 var breatheImageOne = document.getElementById("breatheInImage");
 var breatheButton = document.getElementById("breatheControlButton");
 
 myHeader.innerText = "Hello, " + response;
+myHeader.style.color = "green";
 
-var myheaderColorChange = function () {
+var myHeaderColorChange = function () {
   var redComp = Math.random() * 255;
   console.log(redComp);
   var greenComp = Math.random() * 255;
@@ -15,9 +17,10 @@ var myheaderColorChange = function () {
     "rgb(" + redComp + ", " + greenComp + ", " + blueComp + ")";
 };
 
-var breatheImageSwap = function () {
-  if(breatheInImage.alt == "First breathing step");
+var swapBreatheImage = function () {
+  if(breatheImageOne.alt == "First breathing step");
   breatheImageOne.src = "Images/Java Images-02.jpg";
+
 
 
 }
@@ -31,6 +34,9 @@ else { //otherwise
 
 
 
-
+//just when the JS loads
+myHeaderColorChange();
+//when the button is clicked
 hColorChangeButton.addEventListener("click", myheaderColorChange);
-breatheButton.addEventListener("click", breatheImageSwap);
+
+breatheButton.addEventListener("click", swapBreatheImage);
