016750f648f2178db1081b84512cbe3d562ebb43
chrissycodes2020 <61220882+chrissycodes2020@users.noreply.github.com>
2020-06-16 18:32:09 -0700

    final push!
    push

--- Final/script.js
+++ Final/script.js
@@ -1,3 +1,25 @@
-$(document).ready(function(){
-    
-}
+$(document).ready(function () {
+  $("#learnMore").click(function () {
+    $(".i-text").toggle();
+  });
+
+  $(".donate-button").click(function () {
+    $("form").replaceWith($("#thankYou"));
+  });
+
+  $("#donationB").click(function () {
+    $("#thankYou").show();
+  });
+
+  $(".donate-button").click(function () {
+    $(".replace-form").show();
+  });
+
+  $("#donationB").click(function () {
+    $("form").hide();
+  });
+
+  $(".donate-button").click(function () {
+    $(".replace-form").removeClass("hidden-all");
+  });
+});
